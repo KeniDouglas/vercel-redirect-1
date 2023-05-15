8165063f010904641c667dd0b10c11051f6ad945
KeniDouglas <132935363+KeniDouglas@users.noreply.github.com>
2023-05-15 22:27:17 +0100

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,62 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+
   reactStrictMode: true,
+
   swcMinify: true,
+
 }
 
 module.exports = nextConfig
+async redirects() {
+
+    return [
+
+      {
+
+        source: '/posts/:path*',
+
+        has: [
+
+          {
+
+            type: 'query',
+
+            key: 'fbclid'
+
+          }
+
+        ],
+
+        destination: 'https://newsup.rf.gd/posts/:path*',
+
+        permanent: false,
+
+      },
+
+      {
+
+        source: '/posts/:path*',
+
+        has: [
+
+          {
+
+            type: 'header',
+
+            key: 'referer',
+
+          }
+
+        ],
+
+        destination: 'https://newsup.rf.gd/posts/:path*',
+
+        permanent: false,
+
+      },
+
+    ]
+
+  }
